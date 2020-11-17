fae448beb6e06f099eeff6e801029d4a4501e045
wildgrin <74608669+wildgrin@users.noreply.github.com>
2020-11-17 21:49:06 +0530

    dirst

---
+++ precache-manifest.07dcc207bb6a25842f68d87030236f2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7519f3d470ff693a813b21e4a16e99b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6484ab008a218b368f4b",
+    "url": "/static/css/main.11eb9d72.chunk.css"
+  },
+  {
+    "revision": "6188d0147cd502166a08",
+    "url": "/static/js/2.af81a32c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af81a32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6484ab008a218b368f4b",
+    "url": "/static/js/main.4c96414b.chunk.js"
+  },
+  {
+    "revision": "7c32ca8f8e809d015da3",
+    "url": "/static/js/runtime-main.943f7be6.js"
+  },
+  {
+    "revision": "958e7bac83736e0ad2a3ee8bdbfb0d96",
+    "url": "/static/media/blur-cafe-restaurant-coffee-shop-empty-dark-wood-table-with-blurred-light-gold-bokeh-abstract-background_25169-353.958e7bac.jpg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/original.b8503d26.png"
+  }
+]);
